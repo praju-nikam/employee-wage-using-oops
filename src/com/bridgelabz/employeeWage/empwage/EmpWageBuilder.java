92867218e4d0ed36119eeacde8afcd655bc577bc
praju <sawantprajakta84@gmail.com>
2022-03-19 07:54:56 +0530

    Manage Emp Wage of Multiple Companies Using Interface

--- src/com/bridgelabz/employeeWage/empwage/EmpWageBuilder.java
+++ src/com/bridgelabz/employeeWage/empwage/EmpWageBuilder.java
@@ -93,13 +93,61 @@ public class EmpWageBuilder {
         return totalEmpHrs * companyEmpWage.getEmpRatePerHour();
     }
 
-    public static void main(String[] args) {
-        System.out.println("welcome to employee wage programs");
-        EmpWageBuilder employeeWageBuilder =new EmpWageBuilder();
-        employeeWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
-        employeeWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
-        employeeWageBuilder.computeEmpWage();
+    //Use Case 11
+
+    public static final int IS_PART_TIME=1;
+    public static final int IS_FULL_TIME=2;
+
+    private int numOfCompany=0;
+    private CompanyEmpWage[] companyEmpWageArray;
+
+    public EmployeeWageBuilder() {
+        companyEmpWageArray = new CompanyEmpWage[5];
     }
+
+    public void  addCompanyEmpWage(String company, int empRatePerHour, int numberOfWorkingDays, int maximumHrsInMonth) {
+        companyEmpWageArray[numOfCompany] = new  CompanyEmpWage(company, empRatePerHour, numberOfWorkingDays, maximumHrsInMonth);
+        numOfCompany++;
+    }
+
+    public void computeEmpWage() {
+        for (int i=0; i<numOfCompany; i++) {
+            companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+            System.out.println(companyEmpWageArray[i]);
+        }
+    }
+
+    public  int computeEmpWage(CompanyEmpWage companyEmpWage) {
+        int empHrs=0;
+        int totalWorkingDays=0;
+        int totalEmpHrs=0;
+
+        while (totalEmpHrs <= companyEmpWage.getMaximumHrsInMonth()  && totalWorkingDays < companyEmpWage.getNumberOfWorkingDays()) {
+            totalWorkingDays++;
+            int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day: " + totalWorkingDays + "Emp Hr: " + empHrs);
+        }
+        return totalEmpHrs * companyEmpWage.getEmpRatePerHour();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("welcome to employee wage programms");
+        EmpWageBuilder empWageBuilder =new EmpWageBuilder();
+        empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+        empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+        empWageBuilder.computeEmpWage();    }
 
 
 
